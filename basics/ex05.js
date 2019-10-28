f6870fb6885e2bcf8419181431fc316d1fd5c2d7
AnnaMarandola <annamarandola888@gmail.com>
2019-10-28 22:08:24 +0100

    concaténation

--- basics/ex05.js
+++ basics/ex05.js
@@ -8,20 +8,26 @@
 */
 
 //  écrire votre code sous ce commentaire
-
+function isEvenConcat(num){
+  if(num % 2 == 0){
+    return `${num} est pair`;
+  }else{
+    return `${num} est impair`;
+}
+}
 /*
   Test 1
   Résultat attendu : "6 est pair"
 */
 
-isEvenConcat(6);
+console.log(isEvenConcat(6));
 
 /*
   Test 2
   Résultat attendu : "5 est impair"
 */
 
-isEvenConcat(5);
+console.log(isEvenConcat(5));
 
 /* DO NOT TOUCH */
 module.exports = {
