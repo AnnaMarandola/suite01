8135461cba9dd067ad3a75b7edd2c3e63cf67f05
AnnaMarandola <annamarandola888@gmail.com>
2019-11-02 17:59:08 +0100

    return an a element of an array

--- fundamentals/exo10.js
+++ fundamentals/exo10.js
@@ -12,6 +12,7 @@
 
 //  écrire votre code sous ce commentaire
 function returnElt(arr, pos) {
+  return arr[pos];
 
 }
 
@@ -20,25 +21,25 @@ function returnElt(arr, pos) {
   Test 1:
   Résultat attendu : "Hi"
 */
-returnElt(['Hello', 'Good morning', 'Hey', 'Hi'], 3);
+console.log(returnElt(['Hello', 'Good morning', 'Hey', 'Hi'], 3));
 
 /*
   Test 2:
   Résultat attendu : 1
 */
-returnElt([1, 2, 3, 4], 0);
+console.log(returnElt([1, 2, 3, 4], 0));
 
 /*
   Test 3:
   Résultat attendu : "bananas"
 */
-returnElt(['Mushrooms', 'bananas'], 1);
+console.log(returnElt(['Mushrooms', 'bananas'], 1));
 
 /*
   Test 4:
   Résultat attendu : 51
 */
-returnElt([12, 34, 76, 43, 80, 51, 56, 41, 42], 5);
+console.log(returnElt([12, 34, 76, 43, 80, 51, 56, 41, 42], 5));
 
 
 /* DO NOT TOUCH */
