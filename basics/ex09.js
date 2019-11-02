8a75515d7c1602cbad4469313b8f7b34b1cdcf4d
AnnaMarandola <annamarandola888@gmail.com>
2019-11-02 15:09:46 +0100

    fonction additionant les nombres pairs d'un tableau

--- basics/ex09.js
+++ basics/ex09.js
@@ -6,20 +6,39 @@
 */
 
 //  écrire votre code sous ce commentaire
+function isEven(i){
+  if(i % 2 == 0){
+    return 1;
+  }else{
+    return 0;
+  }
+}
 
+
+
+function boostedEvenAddition (arr){
+  let sumOfEvens = 0;
+  let arrOfEvens = arr.filter(isEven)
+//  console.log(arrOfEvens);
+  for (let i=0; i < arrOfEvens.length; i++){
+    sumOfEvens += arrOfEvens[i]
+  }
+
+  return sumOfEvens;
+}
 /*
   Test 1
   Résultat attendu : 10
 */
 
-boostedEvenAddition([4, 5, 6]);
+console.log(boostedEvenAddition([4, 5, 6]));
 
 /*
   Test 2
   Résultat attendu : 44
 */
 
-boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]);
+console.log(boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]));
 
 /* DO NOT TOUCH */
 module.exports = {
