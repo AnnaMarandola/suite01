f8937c35bbca08cf86938eb0c614d892a81caccf
AnnaMarandola <annamarandola888@gmail.com>
2019-11-02 11:35:40 +0100

    fonction somme des elements d'un tableau

--- basics/ex07.js
+++ basics/ex07.js
@@ -7,18 +7,25 @@
 
 //  écrire votre code sous ce commentaire
 
+function boostedAddition(arr){
+  let total = 0;
+  for (let i= 0; i < arr.length; i++){
+    total += arr[i];
+  }
+  return total;
+}
 /*
   Test 1
   Résultat attendu : 15
 */
 
-boostedAddition([4, 5, 6]);
+console.log(boostedAddition([4, 5, 6]));
 
 /*
   Test 2
   Résultat attendu : 44
 */
-boostedAddition([4, 4, 6, 8, 10, 12]);
+console.log(boostedAddition([4, 4, 6, 8, 10, 12]));
 
 /* DO NOT TOUCH */
 module.exports = {
