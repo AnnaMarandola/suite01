6f0052dbc6b317d23a21f18a7c0bf4d97dcbb06a
AnnaMarandola <annamarandola888@gmail.com>
2019-11-03 14:40:46 +0100

    function modifying a string whith charAt() & replace() methods

--- arrays/exo3.js
+++ arrays/exo3.js
@@ -8,18 +8,22 @@
 */
 
 //  écrire votre code sous ce commentaire
-
+function editStringAt(str1, i, str) {
+  let letterRem = str1.charAt(i);
+  let newstr = str1.replace(letterRem, str)
+  return newstr
+}
 /*
   Test 1
   Résultat attendu : "Bondour"
 */
-editStringAt("Bonjour", 3, "d");
+console.log(editStringAt("Bonjour", 3, "d"));
 
 /*
   Test 2
   Résultat attendu : "Hello World"
 */
-editStringAt("Hello world", 6, "W");
+console.log(editStringAt("Hello world", 6, "W"));
 
 /* DO NOT TOUCH */
 module.exports = {
